89fdac7d457f83045736e13d27c4d4083d163d76
Jessica Andrew <jessica.andrew24@imperial.ac.uk>
2026-03-24 04:54:49 +0200

    Add GitHub actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Loading } from './components/Loading'
 
